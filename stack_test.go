3038d3b1383318aad9a4df4dc3345c968f5a5513
min <min1324@icloud.com>
2022-04-22 23:23:14 +0800

    edit test

--- stack_test.go
+++ stack_test.go
@@ -148,82 +148,82 @@ func TestConcurrent(t *testing.T) {
 	}
 }
 
-func TestLFQueue(t *testing.T) {
-	var d stack.LockFree
-	testPoolPop(t, &d)
-}
+// func TestLFQueue(t *testing.T) {
+// 	var d stack.LockFree
+// 	testPoolPop(t, &d)
+// }
 
-func testPoolPop(t *testing.T, d stack.Stack) {
-	const P = 10
-	var N int = 2e6
-	if testing.Short() {
-		N = 1e3
-	}
-	have := make([]int32, N)
-	var stop int32
-	var wg sync.WaitGroup
-	record := func(val int) {
-		atomic.AddInt32(&have[val], 1)
-		if val == N-1 {
-			atomic.StoreInt32(&stop, 1)
-		}
-	}
+// func testPoolPop(t *testing.T, d stack.Stack) {
+// 	const P = 10
+// 	var N int = 2e6
+// 	if testing.Short() {
+// 		N = 1e3
+// 	}
+// 	have := make([]int32, N)
+// 	var stop int32
+// 	var wg sync.WaitGroup
+// 	record := func(val int) {
+// 		atomic.AddInt32(&have[val], 1)
+// 		if val == N-1 {
+// 			atomic.StoreInt32(&stop, 1)
+// 		}
+// 	}
 
-	// Start P-1 consumers.
-	for i := 1; i < P; i++ {
-		wg.Add(1)
-		go func() {
-			fail := 0
-			for atomic.LoadInt32(&stop) == 0 {
-				val, ok := d.Pop()
-				if ok {
-					fail = 0
-					record(val.(int))
-				} else {
-					// Speed up the test by
-					// allowing the pusher to run.
-					if fail++; fail%100 == 0 {
-						runtime.Gosched()
-					}
-				}
-			}
-			wg.Done()
-		}()
-	}
+// 	// Start P-1 consumers.
+// 	for i := 1; i < P; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			fail := 0
+// 			for atomic.LoadInt32(&stop) == 0 {
+// 				val, ok := d.Pop()
+// 				if ok {
+// 					fail = 0
+// 					record(val.(int))
+// 				} else {
+// 					// Speed up the test by
+// 					// allowing the pusher to run.
+// 					if fail++; fail%100 == 0 {
+// 						runtime.Gosched()
+// 					}
+// 				}
+// 			}
+// 			wg.Done()
+// 		}()
+// 	}
 
-	// Start 1 producer.
-	nPopHead := 0
-	wg.Add(1)
-	go func() {
-		for j := 0; j < N; j++ {
-			for !d.Push(j) {
-				// Allow a popper to run.
-				runtime.Gosched()
-			}
-			if j%10 == 0 {
-				val, ok := d.Pop()
-				if ok {
-					nPopHead++
-					record(val.(int))
-				}
-			}
-		}
-		wg.Done()
-	}()
-	wg.Wait()
+// 	// Start 1 producer.
+// 	nPopHead := 0
+// 	wg.Add(1)
+// 	go func() {
+// 		for j := 0; j < N; j++ {
+// 			for !d.Push(j) {
+// 				// Allow a popper to run.
+// 				runtime.Gosched()
+// 			}
+// 			if j%10 == 0 {
+// 				val, ok := d.Pop()
+// 				if ok {
+// 					nPopHead++
+// 					record(val.(int))
+// 				}
+// 			}
+// 		}
+// 		wg.Done()
+// 	}()
+// 	wg.Wait()
 
-	// Check results.
-	for i, count := range have {
-		if count != 1 {
-			t.Errorf("expected have[%d] = 1, got %d", i, count)
-		}
-	}
-	// Check that at least some PopHeads succeeded. We skip this
-	// check in short mode because it's common enough that the
-	// queue will stay nearly empty all the time and a PopTail
-	// will happen during the window between every PushHead and
-	// PopHead.
-	if !testing.Short() && nPopHead == 0 {
-		t.Errorf("popHead never succeeded")
-	}
-}
+// 	// Check results.
+// 	for i, count := range have {
+// 		if count != 1 {
+// 			t.Errorf("expected have[%d] = 1, got %d", i, count)
+// 		}
+// 	}
+// 	// Check that at least some PopHeads succeeded. We skip this
+// 	// check in short mode because it's common enough that the
+// 	// queue will stay nearly empty all the time and a PopTail
+// 	// will happen during the window between every PushHead and
+// 	// PopHead.
+// 	if !testing.Short() && nPopHead == 0 {
+// 		t.Errorf("popHead never succeeded")
+// 	}
+// }
